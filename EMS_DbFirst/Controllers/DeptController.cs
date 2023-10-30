bf1f14d233c26241960d24a94c7650345be6063f
Iamneo <techadmin@iamneo.ai>
2023-10-30 07:02:24 +0000

    Backup Commit

--- EMS_DbFirst/Controllers/DeptController.cs
+++ EMS_DbFirst/Controllers/DeptController.cs
@@ -13,4 +13,19 @@ public IActionResult List()
     var data=context.Departments.ToList();
     return View(data);
 }
+public IActionResult Create()
+{
+    return View();
+}
+[HttpPost]
+public IActionResult Create(Department department)
+{
+    if(ModelState.IsValid)
+    {
+        context.Departments.Add(department);
+        context.SaveChanges();
+        return RedirectToAction("List");
+    }
+    return View();
+}
 }
