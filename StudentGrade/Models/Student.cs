25b3e61cd269441b0b3d3a9253ce7752815eb63e
Iamneo <techadmin@iamneo.ai>
2023-11-01 07:06:50 +0000

    Backup Commit

--- StudentGrade/Models/Student.cs
+++ StudentGrade/Models/Student.cs
@@ -12,7 +12,6 @@ namespace StudentGrade.Models
         public int StudentId{get;set;}
         public string StudentName{get;set;}
         public DateTime DateOfBirth{get;set;}
-        public byte[] Photo{get;set;}
         public decimal Height{get;set;}
         public float Weight{get;set;}
         public Grade Grade{get;set;}
